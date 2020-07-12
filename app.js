a8fd519e6d76c0771bbccc2866d81cb05258d3ee
Sambud <sbudholiya98@gmail.com>
2020-07-12 20:10:48 +0530

    files added

---
+++ app.js
@@ -0,0 +1,10 @@
+new hoverEffect({
+    parent: document.querySelector('.distortion'),
+    intensity: 0.1,
+    image1: './8.png',
+    image2: './8.png',
+    angle: Math.PI / 4,
+    speedIn: 2,
+    speedOut: 2,
+    displacementImage: './heightMap.png'
+});
